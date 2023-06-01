05bd7eb5a2bf99a0355432de4136e7b51a30eafe
imprint <asiwebrightemeka@gmail.com>
2023-06-01 23:25:47 +0100

    frontend setup

---
+++ src/Components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import Footer from '../Footer/Footer'
+import Nav from '../Navbar/Navbar'
+import { ReactNode } from "react";
+
+
+interface Props{
+    children?:ReactNode
+}
+export const Layout = ({children}:Props) => {
+  return (
+		<Box minHeight={"100vh"} display={"flex"} flexDirection={"column"}>
+			<Box>
+				<Nav />
+			</Box>
+			{children}
+			<Box marginTop={"auto"}>
+				<Footer />
+			</Box>
+		</Box>
+	);
+}
+
+
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,128 @@
+import { ReactNode } from "react";
+import {
+	Box,
+	Flex,
+	Avatar,
+	Link,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	useColorMode,
+    Center,
+    HStack,
+	IconButton,
+	Image,
+	Text
+	
+ 
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Links = [
+	{
+		id: "Home",
+		url: "/",
+	},
+	
+	{
+		id: "About Us",
+		url: "/?query=about-us",
+	},
+	{
+		id: "Our Services",
+		url: "/?query=services",
+	},
+	{
+		id: "Our Features",
+		url: "/?query=features",
+	},
+	{
+		id: "FAQ",
+		url: "/?query=faq",
+	},
+	{
+		id: "Register",
+		url: "/?query=register",
+	},
+	{
+		id: "Login",
+		url: "/?query=login",
+	},
+	
+];
+interface Props {
+    children?: ReactNode,
+	url?: string,
+	
+}
+
+const NavLink = ({ children,url}:Props) => (
+	<Link
+		px={2}
+		py={1}
+		rounded={"md"}
+		fontSize={13}
+		_hover={{
+			textDecoration: "none",
+			bg: useColorModeValue("gray.200", "gray.700"),
+		}}
+		href={url?url:"#"}
+	>
+		{children}
+	</Link>
+);
+
+export default function Nav() {;
+	const { colorMode, toggleColorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+				<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+					<IconButton
+						size={"md"}
+						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						aria-label={"Open Menu"}
+						display={{ md: "none" }}
+						onClick={isOpen ? onClose : onOpen}
+					/>
+					<Box bg={"whiteAlpha.200"} px={4} py={2} borderRadius={"md"}>
+						<Text fontSize={15} fontStyle={"italic"}>Easy Buy</Text>
+					</Box>
+
+					<Flex alignItems={"center"}>
+						<Stack direction={"row"} spacing={7}>
+							<Button onClick={toggleColorMode}>
+								{colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+							</Button>
+						</Stack>
+					</Flex>
+					<HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+						{Links.map((link) => (
+							<Box key={link.id}>
+								<NavLink url={link.url}>{link.id}</NavLink>
+							</Box>
+						))}
+					</HStack>
+				</Flex>
+				{isOpen ? (
+					<Box pb={4} display={{ md: "none" }}>
+						<Stack as={"nav"} spacing={4}>
+							{Links.map((link) => (
+								<Box key={link.id}>
+									<NavLink url={link.url}>{link.id}</NavLink>
+								</Box>
+							))}
+						</Stack>
+					</Box>
+				) : null}
+			</Box>
+		</>
+	);
+}
