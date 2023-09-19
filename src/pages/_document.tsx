9b411d746d527c6ec2380573de7c5163a2b0cf0b
imprint <asiwebrightemeka@gmail.com>
2023-09-19 12:55:39 +0100

    dashboard revamp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8100263567012634"
-          crossorigin="anonymous">  
+          crossOrigin="anonymous">  
         </script>
       </Head>
       <body>
