666a283454a493be1471ac44263b54c08c7d513c
imprint <asiwebrightemeka@gmail.com>
2023-09-26 11:18:28 +0100

    data and airtime refund

--- src/pages/admin/refund/vtpassdata.tsx
+++ src/pages/admin/refund/vtpassdata.tsx
@@ -1,5 +1,6 @@
 import SidebarWithHeader from "@/Components/Dashboard/DashboardWrapper";
-import { VtFailedData } from "@/Components/Admin/Refunds/Data/vtfailedData";
+// import { VtFailedData } from "@/Components/Admin/Refunds/Data/vtfailedData";
+import { VtFailedData } from "@/Components/Admin/Refunds/Data/VtfailedData";
 
 export default function vtpassdata() {
     return (
